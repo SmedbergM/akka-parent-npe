a45a71c97e2196452cf9798aee4a03a0f7e06bfe
berg <matthew.smedberg@gmail.com>
2021-02-03 13:17:22 -0700

    Message both the restarted Level0 and the dead Level1

--- src/main/scala/com/github/smedbergm/MWE.scala
+++ src/main/scala/com/github/smedbergm/MWE.scala
@@ -19,29 +19,39 @@ object MWE extends App {
 }
 
 case class Level0Message(n: Int)
+case class Level0End(n: Int)
+case class Level0InProgress(n: Int)
 
 class Level0 extends Actor with ActorLogging {
   override val receive: Receive = {
     case msg@Level0Message(n) =>
       log.info("Received message {}", msg)
-      val child = context.actorOf(Props(new Level1), s"level-1-${n}")
+      val child = context.actorOf(Props(new Level1(n)), s"level-1-${n}")
       child ! Level1Start
+    case Level0End(n) =>
+      log.info("Task {} completed.", n)
+    case Level0InProgress(n) =>
+      log.info("Task {} is still in progress", n)
   }
 }
 
 case object Level1Start
-sealed trait Level1End
-case class Level1Success(text: String) extends Level1End
-case class Level1Failure(text: String) extends Level1End
+case class Level1Success(text: String)
 
-class Level1 extends Actor with ActorLogging {
+class Level1(n: Int) extends Actor with ActorLogging {
+  import context.dispatcher
+
   override val receive: Receive = {
     case Level1Start =>
       log.info("Received {}", Level1Start)
       val child = context.actorOf(Props(new Level2), "leaf-actor")
       child ! Level2Message(context.self.path.hashCode())
-    case msg: Level1End =>
+      context.system.scheduler.scheduleOnce(1.second) {
+        context.parent ! Level0InProgress(n)
+      }
+    case msg: Level1Success =>
       log.info("Received {}", msg)
+      context.parent ! Level0End(n)
   }
 }
 
@@ -52,14 +62,8 @@ class Level2 extends Actor {
 
   override val receive: Receive = {
     case Level2Message(n) =>
-      val p: Promise[String] = Promise()
       context.system.scheduler.scheduleOnce(1.second) {
-        val succeeded = p.trySuccess(s"success-${n}")
-        if (succeeded) {
-          context.parent ! Level1Success("success")
-        } else {
-          context.parent ! Level1Failure("failure")
-        }
+        context.parent ! Level1Success(s"success-${n}")
       }
   }
 }
